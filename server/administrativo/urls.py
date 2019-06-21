408d2eb6b4fa0fae9c3bf4046eac515f145a87d1
pedrodaltro <pedro.daltro@gmail.com>
2019-06-21 11:53:34 -0300

    Ajuste para o .gitignore funcionar

--- server/administrativo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import *
-from rest_framework_jwt.views import obtain_jwt_token
-
-urlpatterns = [
-    path('login/', obtain_jwt_token),
-    path('usuarios/', UsuarioView.as_view({'get': 'list'}), name='api-usuarios'),
-]
