33a9d873d4f18ced7ba30b1cff703502b75fd6bc
Igor Gonçalves <igor_goncalves@live.com>
2019-07-22 14:01:41 -0300

    Banner de busca - tela principal

--- client/components/organisms/LoginForm.vue
+++ client/components/organisms/LoginForm.vue
@@ -4,7 +4,7 @@
       <b-field
         label="Email"
         :type="{'is-danger': errors.has('email')}"
-        :message="errors.first('email')"        
+        :message="errors.first('email')"
       >
         <b-input type="text" v-model="email" name="email" v-validate="'required|email'" />
       </b-field>
@@ -13,35 +13,34 @@
         label="Password"
         :type="{'is-danger': errors.has('password')}"
         :message="errors.first('password')"
-      >      
+      >
         <b-input
-            type="password" 
-            name="password" 
-            password-reveal
-            v-model="password" 
-            v-validate="'required|min:8'" />
+          type="password"
+          name="password"
+          password-reveal
+          v-model="password"
+          v-validate="'required|min:8'"
+        />
       </b-field>
       <div class="column">
-         Não é usuário? <nuxt-link
-                class=" is-primary is-inverted"
-                to="/cadastro"
-                exact-active-class="is-active"
-              >
-                <!-- <b-icon :icon="item.icon" /> {{ item.title }} -->
-                Cadastre-se
-              </nuxt-link>        
-        </div>
-      <br>
-      
-     <button type="submit" class="button is-primary is-outlined is-medium is-rounded is-fullwidth"> Entrar</button>
-     <div class="column  has-text-centered">
-      <nuxt-link
+        Não é usuário?
+        <nuxt-link class="is-primary is-inverted" to="/cadastro" exact-active-class="is-active">
+          <!-- <b-icon :icon="item.icon" /> {{ item.title }} -->
+          Cadastre-se
+        </nuxt-link>
+      </div>
+      <br />
+
+      <button
+        type="submit"
+        class="button is-primary is-outlined is-medium is-rounded is-fullwidth"
+      >Entrar</button>
+      <div class="column has-text-centered">
+        <nuxt-link
           class="voltar is-primary is-inverted"
           to="/"
           exact-active-class="is-active"
-        >                      
-          Voltar
-        </nuxt-link>     
+        >Voltar</nuxt-link>
       </div>
     </form>
   </section>
@@ -56,43 +55,43 @@ export default {
     }
   },
   methods: {
-    async login () {
-        try {
-            this.$auth.loginWith('local', {
-                data: {
-                    email: this.email,
-                    password: this.password
-                }
-            }).catch((err) => {
-              console.error(err);
-              if(! err.response){
-                err.message = "Servidor desconectado"
-              } 
-              else if(err.response.status === 400){
-                err.message = err.response.data.non_field_errors[0]
-              }              
-              this.$toast.open({
-                message: err.message,
-                type: 'is-danger',
-                position: 'is-bottom'
-              })     
+    async login() {
+      try {
+        this.$auth
+          .loginWith('local', {
+            data: {
+              email: this.email,
+              password: this.password
+            }
+          })
+          .catch(err => {
+            console.error(err)
+            if (!err.response) {
+              err.message = 'Ops! O servidor de login está desconectado.'
+            } else if (err.response.status === 400) {
+              err.message = err.response.data.non_field_errors[0]
+            }
+            this.$toast.open({
+              message: err.message,
+              type: 'is-danger',
+              position: 'is-bottom'
             })
-            // this.$router.push('/')
-        } catch (e) {        
-         this.$toast.open({
+          })
+        // this.$router.push('/')
+      } catch (e) {
+        this.$toast.open({
           message: e.response.data.message,
           type: 'is-danger',
-          position: 'is-bottom'        
+          position: 'is-bottom'
         })
-        
+
         return
-        
       }
     },
 
     validateBeforeSubmit() {
       this.$validator.validateAll().then(result => {
-        if (result) {          
+        if (result) {
           this.login()
           return
         }
