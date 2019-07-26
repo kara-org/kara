385ed55c2d3829abb4cc5fb6ea9444ff5333fae6
Igor Gonçalves <igor_goncalves@live.com>
2019-07-26 01:41:10 -0300

    Layout + Menu lateral

--- client/components/organisms/NavBar.vue
+++ client/components/organisms/NavBar.vue
@@ -58,3 +58,22 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+.navbar {
+  color: $primary;
+
+  .navbar-brand {
+    padding: 10px
+  }
+
+
+  &::after {
+    background: linear-gradient(-87deg, rgba(26,188,156, 0.8) 0%,  rgba(46,204,113,0.8) 100%);
+    bottom: 0;
+    content: '';
+    height: 3px;
+    position: absolute;
+    width: 100%;
+  }
+}
+</style>
