4fadff76b5d9bde599f17fb08635b2bd01f68cf3
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2019-09-18 03:43:00 -0300

    dart test, organização de packages e identação

--- app/lib/src/interfaces/i_access_token.dart
+++ app/lib/src/interfaces/i_access_token.dart
@@ -1,6 +1,8 @@
-import 'i_service.dart';
-import '../models/access_token.dart';
 import 'package:meta/meta.dart';
+
+import 'package:kara/src/models/access_token.dart';
+
+import 'i_service.dart';
 
 abstract class IAccessToken implements IService<AccessToken> {
   Future<AccessToken> getAccessToken(
