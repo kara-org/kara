5f6e24cbf9073905373b30d3a00935af99f8d5cb
Mayara Machado <mayara@localhost.localdomain>
2019-11-11 23:43:32 -0300

    Inserts em python de valores defaults

---
+++ server/kara/db_defaults_values.py
@@ -0,0 +1,22 @@
+from doacoes.models import Categoria, StatusItemDoacao
+
+StatusItemDoacao.objects.bulk_create(
+    [ 
+        StatusItemDoacao(codigo_status=1, mensagem="Pendente"),
+        StatusItemDoacao(codigo_status=2, mensagem="Cancelada"),
+        StatusItemDoacao(codigo_status=3, mensagem="Confirmada")
+    ] 
+)
+
+Categoria.objects.bulk_create(
+    [
+        Categoria(descricao="Alimentos"),
+        Categoria(descricao="Roupas"),
+        Categoria(descricao="Móveis"),
+        Categoria(descricao="Saúde"),
+        Categoria(descricao="Serviços"),
+        Categoria(descricao="Dinheiro"),
+        Categoria(descricao="Eletrodoméstico"),
+        Categoria(descricao="Aparelhos tecnológicos"),
+    ]
+)
